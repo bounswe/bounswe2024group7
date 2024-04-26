895ebb1850ee2ad8fe907e06371a1fb2d321cbab
Samet Alan <a.sametalan56@gmail.com>
2024-04-26 18:53:33 +0300

    Updates

---
+++ assets/index.lazy-x4_mghRH.js
@@ -0,0 +1,1 @@
+import{k as o,j as e}from"./index-I09gVcOS.js";const t="/bounswe2024group7/",r=o(t)({component:n});function n(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{r as Route};
