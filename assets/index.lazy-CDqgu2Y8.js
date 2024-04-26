11846701a45117cb489f22a196717359932af5a2
Samet Alan <a.sametalan56@gmail.com>
2024-04-26 18:33:10 +0300

    Updates

---
+++ assets/index.lazy-CDqgu2Y8.js
@@ -0,0 +1,1 @@
+import{k as o,j as e}from"./index-B_ZACgGZ.js";const r=o("/")({component:t});function t(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{r as Route};
