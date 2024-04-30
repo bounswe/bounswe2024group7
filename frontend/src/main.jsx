4df51fb4500282e4072d142b25475f38f06566a5
erenpakelgil <eren.pakelgil@std.bogazici.edu.tr>
2024-04-30 21:16:29 +0300

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import { rootRoute } from "./routes/__root"
 import { indexRoute } from './routes/index'
 import { loginRoute } from './routes/login'
 import { registerRoute } from './routes/register'
-import { registerRoute } from './routes/search_results'
+import { searchResultsRoute } from './routes/search_results'
 
 
 const routeTree = rootRoute.addChildren([
--- frontend/src/routes/search_resuls.jsx
+++ frontend/src/routes/search_resuls.jsx
@@ -2,7 +2,7 @@ import { createLazyFileRoute, createRoute } from '@tanstack/react-router'
 import SearchResultsComponent from '../components/searchResults.component'
 import { rootRoute } from "./__root"
 
-export const registerRoute = createRoute({
+export const searchResultsRoute = createRoute({
     getParentRoute: () => rootRoute,
     path: "/search",
     component: SearchResults
