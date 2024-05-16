0ebe96b1c2a1ea0e09644d809cf7927dbbef41be
hanazaq <hanaa2002hana@gmail.com>
2024-05-16 21:32:01 +0300

    Update Feed.js

--- artifact_MobileApp/Feed.js
+++ artifact_MobileApp/Feed.js
@@ -10,35 +10,36 @@ const Feed = () => {
             "material": "Oil on poplar",
             "genre": "Portrait",
             "creator": "Leonardo da Vinci"
-        },
-        {
-            "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
-            "title": "Starry Night",
-            "material": "Oil on canvas",
-            "genre": "Post-Impressionism",
-            "creator": "Vincent van Gogh"
-        },
-        {
-            "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
-            "title": "The Persistence of Memory",
-            "material": "Oil on canvas",
-            "genre": "Surrealism",
-            "creator": "Salvador Dalí"
-        },
-        {
-            "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
-            "title": "The Scream",
-            "material": "Oil, tempera, and pastel on cardboard",
-            "genre": "Expressionism",
-            "creator": "Edvard Munch"
-        },
-        {
-            "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
-            "title": "Girl with a Pearl Earring",
-            "material": "Oil on canvas",
-            "genre": "Tronie",
-            "creator": "Johannes Vermeer"
         }
+        // ,
+        // {
+        //     "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
+        //     "title": "Starry Night",
+        //     "material": "Oil on canvas",
+        //     "genre": "Post-Impressionism",
+        //     "creator": "Vincent van Gogh"
+        // },
+        // {
+        //     "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
+        //     "title": "The Persistence of Memory",
+        //     "material": "Oil on canvas",
+        //     "genre": "Surrealism",
+        //     "creator": "Salvador Dalí"
+        // },
+        // {
+        //     "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
+        //     "title": "The Scream",
+        //     "material": "Oil, tempera, and pastel on cardboard",
+        //     "genre": "Expressionism",
+        //     "creator": "Edvard Munch"
+        // },
+        // {
+        //     "imageURL": "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQsu7yYuRPXNK9eHHSFD2tUYO4stQDb1Ez8vjqGERfs9xqYLLnY_y6lQkPFZa-44cqn",
+        //     "title": "Girl with a Pearl Earring",
+        //     "material": "Oil on canvas",
+        //     "genre": "Tronie",
+        //     "creator": "Johannes Vermeer"
+        // }
     ];
 
     return (
