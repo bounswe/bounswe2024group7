1ba76b839799f7c0c193bc44a5ae81263df03710
erenpakelgil <eren.pakelgil@std.bogazici.edu.tr>
2024-05-16 20:15:24 +0300

    Fixed a bug

--- artifact_backend/artifactapp/views/auth.py
+++ artifact_backend/artifactapp/views/auth.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from rest_framework import status, views
 from rest_framework.response import Response
-from .serializers import UserSerializer, LoginSerializer
-from .search import get_painting_sparql, get_movement_sparql, get_genre_sparql, related_search
+from ..serializers import UserSerializer, LoginSerializer
+from .search import get_painting_sparql, get_movement_sparql, get_genre_sparql
 from rest_framework.decorators import api_view
 
 class SignupView(views.APIView):
@@ -13,21 +13,25 @@ class SignupView(views.APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LoginView(views.APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+            user = authenticate(username=serializer.validated_data['username'],
+                                password=serializer.validated_data['password'])
             if user:
                 login(request, user)
                 return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
             return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LogoutView(views.APIView):
     def post(self, request):
         logout(request)
         return Response({"message": "Logged out successfully"}, status=status.HTTP_200_OK)
+
 
 class HealthView(views.APIView):
     def get(self, request):
@@ -48,13 +52,10 @@ def artwork_search(request):
             painting_results = get_painting_sparql(query)
             movement_results = get_movement_sparql(query)
             genre_results = get_genre_sparql(query)
-            related_results = related_search(query)
-
             return Response({
                 'painting_results': painting_results,
                 'movement_results': movement_results,
-                'genre_results': genre_results,
-                'related_results':related_results
+                'genre_results': genre_results
             }, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
