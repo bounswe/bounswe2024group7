312151c8563feb525939eb3b5b1a097fc53bdc72
erenpakelgil <eren.pakelgil@std.bogazici.edu.tr>
2024-05-16 19:49:44 +0300

    Modified

--- artifact_backend/artifactapp/search.py
+++ artifact_backend/artifactapp/search.py
@@ -58,70 +58,80 @@ def related_search(keyword):
     
 
 def get_painting_sparql(keyword):
-    keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
-    sparql.setQuery(f"""
-    SELECT ?itemLabel ?image ?creatorLabel ?genreLabel ?materialLabel ?creator
-    WHERE {{
-        ?item rdfs:label ?itemLabel.
-        FILTER(REGEX(?itemLabel, "{keyword}"@en, "i")).
-        ?item wdt:P31 wd:Q3305213.
-        ?item wdt:P18 ?image.
-        ?item wdt:P136 ?genre.
-        ?item wdt:P170 ?creator.
-        ?item wdt:P186 ?material.
-        FILTER(LANG(?itemLabel)="en").
-        BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId).
-
-        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
-    }}
-    """ )
-    try:
-       # for r in sparql.queryAndConvert()["results"]["bindings"]:
-        #    print(r["itemLabel"]["value"])
-        return sparql.queryAndConvert()["results"]["bindings"] 
-    except Exception as e:
-        return []
+    #keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
+    possibilities=generate_combinations(keyword.split())
+    for i in possibilities:
+        sparql.setQuery(f"""
+        SELECT ?itemLabel ?image ?creatorLabel ?genreLabel ?materialLabel ?creator
+        WHERE {{
+            ?item rdfs:label ?itemLabel.
+            ?item rdfs:label "{i}"@en.
+            ?item wdt:P31 wd:Q3305213.
+            ?item wdt:P18 ?image.
+            ?item wdt:P136 ?genre.
+            ?item wdt:P170 ?creator.
+            ?item wdt:P186 ?material.
+            FILTER(LANG(?itemLabel)="en").
+            BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId).
+            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "en". }}
+        }}
+        """ )
+        try:
+        # for r in sparql.queryAndConvert()["results"]["bindings"]:
+            #    print(r["itemLabel"]["value"])
+            results = sparql.queryAndConvert()["results"]["bindings"] 
+            if len(results) == 0:
+                continue
+            else:
+                return results
+        except Exception as e:
+            continue
+    return []
 
 def get_movement_sparql(keyword):
-    keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
-    sparql.setQuery(f"""
-    SELECT ?item ?itemLabel ?itemId
-    WHERE {{
-        ?item rdfs:label ?itemLabel.
-        FILTER(REGEX(?itemLabel, "{keyword}","i")).
-        ?item wdt:P31 wd:Q968159.
-        FILTER(LANG(?itemLabel)="en").
-        BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId)
-    }}
-    """ )
-    if len(sparql.queryAndConvert()["results"]["bindings"]) != 0:
-        lst = []
-        possible_ids = sparql.queryAndConvert()["results"]["bindings"]
-        for possible_id in possible_ids:
-            item_id = possible_id["itemId"]["value"]
-            lst.extend(find_paintings(135, item_id))
-        return lst
+    #keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
+    possibilities=generate_combinations(keyword.split())
+    for i in possibilities:
+        sparql.setQuery(f"""
+        SELECT ?item ?itemLabel ?itemId
+        WHERE {{
+            ?item rdfs:label ?itemLabel.
+            ?item rdfs:label "{i}"@en.
+            ?item wdt:P31 wd:Q968159.
+            FILTER(LANG(?itemLabel)="en").
+            BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId)
+        }}
+        """ )
+        if len(sparql.queryAndConvert()["results"]["bindings"]) != 0:
+            lst = []
+            possible_ids = sparql.queryAndConvert()["results"]["bindings"]
+            for possible_id in possible_ids:
+                item_id = possible_id["itemId"]["value"]
+                lst.extend(find_paintings(135, item_id))
+            return lst
     return []
 
 def get_genre_sparql(keyword):
-    keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
-    sparql.setQuery(f"""
-    SELECT ?item ?itemLabel ?itemId
-    WHERE {{
-        ?item rdfs:label ?itemLabel.
-        FILTER(REGEX(?itemLabel, "{keyword}","i")).
-        ?item wdt:P31 wd:Q16743958.
-        FILTER(LANG(?itemLabel)="en").
-        BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId)
-    }}
-    """ )
-    if len(sparql.queryAndConvert()["results"]["bindings"]) != 0:
-        lst = []
-        possible_ids = sparql.queryAndConvert()["results"]["bindings"]
-        for possible_id in possible_ids:
-            item_id = possible_id["itemId"]["value"]
-            lst.extend(find_paintings(136, item_id))
-        return lst
+    #keyword = f"[^a-zA-Z]{keyword}[^a-zA-Z]|^{keyword}[^a-zA-Z]|[^a-zA-Z]{keyword}$|^{keyword}$"
+    possibilities=generate_combinations(keyword.split())
+    for i in possibilities: 
+        sparql.setQuery(f"""
+        SELECT ?item ?itemLabel ?itemId
+        WHERE {{
+            ?item rdfs:label ?itemLabel.
+            ?item rdfs:label "{i}"@en.
+            ?item wdt:P31 wd:Q16743958.
+            FILTER(LANG(?itemLabel)="en").
+            BIND(REPLACE(STR(?item), "http://www.wikidata.org/entity/", "") AS ?itemId)
+        }}
+        """ )
+        if len(sparql.queryAndConvert()["results"]["bindings"]) != 0:
+            lst = []
+            possible_ids = sparql.queryAndConvert()["results"]["bindings"]
+            for possible_id in possible_ids:
+                item_id = possible_id["itemId"]["value"]
+                lst.extend(find_paintings(136, item_id))
+            return lst
     return []
 
 
@@ -182,3 +192,28 @@ def test():
 
 #Uncomment the row below to test the semantic search feature
 #test()  
+
+
+
+def generate_combinations(words, index=0, current_combination=None, result=None):
+    if result is None:
+        result = []
+    if current_combination is None:
+        current_combination = []
+    
+    if index == len(words):
+        # Join the current combination into a single string and add to the result list
+        result.append(' '.join(current_combination))
+        return result
+    
+    word = words[index]
+    # Generate two versions of the current word: one with a capital first letter and one with a small first letter
+    lower_first = word[0].lower() + word[1:]
+    upper_first = word[0].upper() + word[1:]
+    
+    # Recur with the next word, adding the lower-case version of the current word
+    generate_combinations(words, index + 1, current_combination + [lower_first], result)
+    # Recur with the next word, adding the upper-case version of the current word
+    generate_combinations(words, index + 1, current_combination + [upper_first], result)
+    
+    return result
