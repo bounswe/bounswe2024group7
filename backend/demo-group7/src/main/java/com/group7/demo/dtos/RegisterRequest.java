448c6a0ed3153e6838d512a874e963e6d4f43288
oguzhekim <mustafaoguzhekim@gmail.com>
2024-10-18 19:04:21 +0300

    Created login/register dto

---
+++ backend/demo-group7/src/main/java/com/group7/demo/dtos/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.group7.demo.dtos;
+
+import com.group7.demo.models.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String fullName;
+    private String email;
+    private String username;
+    private String password;
+    private Role role;
+}
