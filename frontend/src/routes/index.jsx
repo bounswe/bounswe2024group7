574943a2a8a4e24288a93f2ef77dbba81460650b
Samet Alan <a.sametalan56@gmail.com>
2024-10-21 13:56:35 +0300

    Update index.jsx

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,7 +2,7 @@ import { createFileRoute, createRoute } from '@tanstack/react-router'
 import { rootRoute } from './__root'
 import { Component } from 'react'
 import { PhaseContextProvider } from '../context/PostContext'
-import PostFeed from '../components/PostFeed,component'
+import PostFeed from '../components/PostFeed.component'
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
