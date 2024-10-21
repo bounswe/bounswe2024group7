c5d27b4461f4280806f90c95e15f90a2aeef1dbe
Modjak <mustafaocak@outlook.com.tr>
2024-10-22 00:42:49 +0300

    delete unused service

--- backend/demo-group7/src/main/java/com/group7/demo/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group7.demo.repository;
-
-import com.group7.demo.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
