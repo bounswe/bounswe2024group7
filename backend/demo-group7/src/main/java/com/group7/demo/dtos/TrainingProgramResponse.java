705017801eb9b96fcbef4b950e1814d5cf17c960
oguzhekim <mustafaoguzhekim@gmail.com>
2024-11-14 19:06:30 +0300

    Change structure of models and dtos

--- backend/demo-group7/src/main/java/com/group7/demo/dtos/TrainingProgramResponse.java
+++ backend/demo-group7/src/main/java/com/group7/demo/dtos/TrainingProgramResponse.java
@@ -1,12 +1,10 @@
 package com.group7.demo.dtos;
 
-import com.group7.demo.models.enums.LocationType;
-import com.group7.demo.models.enums.ProgramType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -15,9 +13,9 @@ import java.util.List;
 public class TrainingProgramResponse {
     private Long id;
     private String title;
-    private ProgramType programType;
-    private LocationType locationType;
-    private List<ExerciseResponse> exercises;
+    private List<ExerciseDetail> exercises;
     private String description;
     private String trainerUsername;
+    private List<String> participants;
+    private LocalDateTime createdAt;
 }
