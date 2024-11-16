4c8a0371f5b4782e9b451f25438353d84b47fb5d
Modjak <mustafaocak@outlook.com.tr>
2024-11-16 03:54:24 +0300

    base url change

--- archive/artifact_MobileApp/Api.js
+++ archive/artifact_MobileApp/Api.js
@@ -5,7 +5,9 @@ import { useState } from "react";
 // todo: would be the best case scenario but we are in a hurry
 
 // const baseURL = "http://10.0.2.2:8081"
-const baseURL = "https://artifactbackend-yslcfqdwna-oa.a.run.app"
+// const baseURL = "https://artifactbackend-yslcfqdwna-oa.a.run.app"
+
+const baseURL = 'http://165.227.166.132:30002' // Replace with your actual base URL
 
 function apiInstance() {
   return axios.create({
--- frontend/src/instance/apiInstance.js
+++ frontend/src/instance/apiInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL 
+const baseURL = import.meta.env.VITE_API_URL
 
 function apiInstance(
  sessionToken,
