13c7984456484b633409065e01d1f028a0d6dc78
oguzhekim <mustafaoguzhekim@gmail.com>
2024-12-13 16:13:35 +0300

    Create exercise based progress endpoint

--- backend/demo-group7/src/main/java/com/group7/demo/repository/WorkoutExerciseWithTrackingRepository.java
+++ backend/demo-group7/src/main/java/com/group7/demo/repository/WorkoutExerciseWithTrackingRepository.java
@@ -1,7 +1,11 @@
 package com.group7.demo.repository;
 
+import com.group7.demo.models.User;
 import com.group7.demo.models.WorkoutExerciseWithTracking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutExerciseWithTrackingRepository extends JpaRepository<WorkoutExerciseWithTracking, Long> {
+    List<WorkoutExerciseWithTracking> findAllByWorkoutExercise_Exercise_IdAndWorkoutWithTracking_WeekWithTracking_TrainingProgramWithTracking_User(Long exerciseId, User user);
 }
