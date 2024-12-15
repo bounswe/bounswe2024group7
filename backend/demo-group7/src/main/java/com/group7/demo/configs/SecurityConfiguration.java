7535cd2f534cbc8a2e3a605e829e00db87715140
oguzhekim <mustafaoguzhekim@gmail.com>
2024-12-15 17:15:18 +0300

    Update security config

--- backend/demo-group7/src/main/java/com/group7/demo/configs/SecurityConfiguration.java
+++ backend/demo-group7/src/main/java/com/group7/demo/configs/SecurityConfiguration.java
@@ -41,7 +41,8 @@ public class SecurityConfiguration {
                             "/api/posts/**",
                             "/api/tags",
                             "/api/training-programs/**",
-                            "/api/exercises/*"
+                            "/api/exercises/*",
+                            "api/exercises"
                     ).permitAll()
                     .anyRequest().authenticated() // All other requests require authentication
             )
