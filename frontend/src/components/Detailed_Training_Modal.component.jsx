0714cb3c8455fd832fc112b7b23696b961a001f1
hanazaq <hanaa2002hana@gmail.com>
2024-12-16 09:10:22 +0300

    now you can navigate to training page from training modal!

--- frontend/src/components/Detailed_Training_Modal.component.jsx
+++ frontend/src/components/Detailed_Training_Modal.component.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button } from '@chakra-ui/react';
-import { Table, TableCaption, Thead, Tbody, Tfoot, Tr, Th, Td, UnorderedList, ListItem } from '@chakra-ui/react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    UnorderedList,
+    ListItem,
+} from '@chakra-ui/react';
+import { useNavigate } from '@tanstack/react-router';
+import { LinkIcon } from '@chakra-ui/icons';
 
 const Detailed_Training_Modal = ({ isOpen, onClose, data }) => {
+    const navigate = useNavigate();
+
+    const VisitTraining = (program_id) => {
+        navigate({
+            to: `/training?trainingId=${program_id}`,
+        });
+    };
+
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
@@ -10,8 +36,7 @@ const Detailed_Training_Modal = ({ isOpen, onClose, data }) => {
                 <ModalHeader>{data.title} Detailed Description</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    <Table variant='striped' width="100%">
-
+                    <Table variant="striped" width="100%">
                         <Thead>
                             <Tr>
                                 <Th>Week</Th>
@@ -20,34 +45,38 @@ const Detailed_Training_Modal = ({ isOpen, onClose, data }) => {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {data.weeks.map((week) => (
+                            {data.weeks.map((week) =>
                                 week.workouts.map((workout) => (
                                     <Tr key={workout.id}>
                                         <Td>Week {week.weekNumber}</Td>
                                         <Td>{workout.name}</Td>
                                         <Td>
-                                            <UnorderedList key={workout.id}>
+                                            <UnorderedList>
                                                 {workout.workoutExercises.map((exerciseob) => (
-                                                    <ListItem key={exerciseob.id} textAlign="left">{exerciseob.exercise.name}</ListItem>
+                                                    <ListItem key={exerciseob.id} textAlign="left">
+                                                        {exerciseob.exercise.name}
+                                                    </ListItem>
                                                 ))}
-                                            </UnorderedList></Td>
+                                            </UnorderedList>
+                                        </Td>
                                     </Tr>
                                 ))
-                            ))}
-
-
+                            )}
                         </Tbody>
-                        <Tfoot>
-
-                        </Tfoot>
                     </Table>
                 </ModalBody>
-
                 <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
                         Close
                     </Button>
-                    {/* <Button variant='ghost'>Secondary Action</Button> */}
+                    <Button
+                        // variant="gohst"
+                        colorScheme="green"
+                        onClick={() => VisitTraining(data.id)} // Pass a function reference
+                    >
+                        <LinkIcon className="w-4 h-4 mr-3" />
+                        Visit Training Page
+                    </Button>
                 </ModalFooter>
             </ModalContent>
         </Modal>
